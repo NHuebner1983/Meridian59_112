10096262f3bcc31ab0fdbb87c9e1f2fda39538de
Nathan Huebner <ndallas1983@gmail.com>
2018-03-01 17:58:37 -0600

    An attempt to simplify the nmake-edit-me.bat file. Now supports automatically copying the RSB files to your bin resource folders for Meridian 59.

--- nmake-edit-me.bat
+++ nmake-edit-me.bat
@@ -1,25 +1,96 @@
 echo off
 
-echo Set next line of this script to your Disk containing your M59 folder.
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM %        Configure your current Visual Studio vcvarsall.bat location         %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-	D:
+	SET "VISUAL_STUDIO_DRIVE=D"
 
-echo Set next line of this script to your M59 folder.
+	SET "VCVBAT=%VISUAL_STUDIO_DRIVE%:\Visual Studio 2015\VC\vcvarsall.bat"
 
-	cd Meridian59
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM % Configure your Development Meridian 59 Drive, Directory and OgreClient Bin %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-echo Set next line of this script to the vcvarsall.bat. If you cannot find this file, it usually comes with Visual Studio 2015.
+	SET "M59_DRIVE=D"
 
-	call "D:\Visual Studio 2015\VC\vcvarsall.bat" x86
+	SET "M59_FOLDER=Meridian59"
 
-echo "This command should build blakserv and perform all of the heavy lifting such as KOD updates."
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM %                   Optional M59 OgreClient Bin (KODers)                     %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	SET "M59_OGRE_BIN=%M59_DRIVE%:\%M59_FOLDER%\_OgreClient\Meridian59.Ogre.Client\bin"
+
+
+
+
+REM ###########################################################################
+REM #         You shouldn't have to touch anything below this line            #
+REM ###########################################################################
+
+
+
+
+
+SET "EDIT_VERBIAGE=Edit this script and try again."
+
+IF NOT EXIST "%VISUAL_STUDIO_DRIVE%:" (
+	ECHO The Visual Studio drive %VISUAL_STUDIO_DRIVE% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%VCVBAT%" (
+	ECHO The Visual Studio file %VCVBAT% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%M59_DRIVE%": (
+	ECHO The drive %M59_DRIVE%:\ does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%M59_DRIVE%:\%M59_FOLDER%" (
+	ECHO The folder %M59_DRIVE%:\%M59_FOLDER% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+
+IF NOT EXIST "%M59_OGRE_BIN%" (
+	ECHO Notice: M59 Ogre Bin was not found. When you want your RSB strings updated to your client, be sure to update this path.
+	pause
+	exit
+)
+
+echo Your directories are setup correctly.
+echo Starting the Visual Studio vcvarsall.bat script...
+echo Please wait...
+
+	%M59_DRIVE%:
+	cd /
+	cd %M59_FOLDER%
+	call "%VCVBAT%" x86
+
+echo Building your Meridian 59 Solution...
+echo on
 
 	nmake debug=1
 
-echo ----------------------------
-echo All finished!
-echo ----------------------------
-echo on
+echo off
+echo The operation has completed.
+echo If you're satisfied, press any key to close this window.
+
+
+	IF EXIST "%M59_OGRE_BIN%" (
+		IF EXIST "%M59_OGRE_BIN%\x86\resources\strings" (
+			echo Destination: %M59_OGRE_BIN%\x86\resources\strings\
+			copy /Y "%M59_DRIVE%:\%M59_FOLDER%\run\server\rsc\rsc*.rsb" "%M59_OGRE_BIN%\x86\resources\strings\"
+		)
+		IF EXIST "%M59_OGRE_BIN%\x64\resources\strings" (
+			echo Destination: %M59_OGRE_BIN%\x64\resources\strings\
+			copy /Y "%M59_DRIVE%:\%M59_FOLDER%\run\server\rsc\rsc*.rsb" "%M59_OGRE_BIN%\x64\resources\strings\"
+		)
+	)
 
 	pause
 
