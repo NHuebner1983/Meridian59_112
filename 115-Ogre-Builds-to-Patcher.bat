e41c8e72bf930b2b331ba1d70883b95fadf0feb9
Nathan Huebner <ndallas1983@gmail.com>
2018-03-07 21:49:04 -0600

    AUTOMATION: Created a BAT file to copy all of my Ogre Release files (x86/x64) to the _Server115 folder which will then need to be committed to GitHub and then pulled down on the server running it. After that, the patcher needs to have a PHP script run to update the files for users.

---
+++ 115-Ogre-Builds-to-Patcher.bat
@@ -0,0 +1,82 @@
+echo off
+
+
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM % Configure your Development Meridian 59 Drive, Directory and OgreClient Bin %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	SET "M59_DRIVE=D"
+	SET "M59_FOLDER=Meridian59"
+
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM %                   Optional M59 OgreClient Bin (KODers)                     %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	SET "M59_OGRE_BASE=%M59_DRIVE%:\%M59_FOLDER%\_OgreClient"
+	SET "M59_OGRE_BIN=%M59_OGRE_BASE%\Meridian59.Ogre.Client\bin"
+	SET "M59_OGRE_PATCHER_BIN=%M59_OGRE_BASE%\Meridian59.Patcher\bin\x86\Release"
+	SET "M59_OGRE_SETUP_BIN=%M59_OGRE_BASE%\Meridian59.Ogre.Setup\Release"
+
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM %                   Patcher Folder (To Upload or Commit                      %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	
+	SET "M59_PATCH_FOLDER=%M59_DRIVE%:\%M59_FOLDER%\_Server115"
+	SET "OGRE_PATCH_FOLDER=%M59_PATCH_FOLDER%\ogrepatch"
+	SET "OGRE_SETUP_FOLDER=%M59_PATCH_FOLDER%\download"
+	
+REM ###########################################################################
+REM #         You shouldn't have to touch anything below this line            #
+REM ###########################################################################
+
+
+
+SET "EDIT_VERBIAGE=Edit this script and try again."
+
+IF NOT EXIST "%M59_DRIVE%": (
+	ECHO The drive %M59_DRIVE%:\ does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%M59_DRIVE%:\%M59_FOLDER%" (
+	ECHO The folder %M59_DRIVE%:\%M59_FOLDER% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%M59_OGRE_BIN%" (
+	ECHO The folder %M59_OGRE_BIN% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+IF NOT EXIST "%OGRE_PATCH_FOLDER%" (
+	ECHO The folder %OGRE_PATCH_FOLDER% does not exist. %EDIT_VERBIAGE%
+	pause
+	exit
+)
+
+echo Your directories are setup correctly.
+
+	IF EXIST "%M59_OGRE_BIN%" (
+		
+		echo Copying x86 and x64 Ogre Client files...
+		
+		IF EXIST "%M59_OGRE_BIN%\x86\Release" (
+			echo Copying: %M59_OGRE_BIN%\x86\Release\...
+			copy /Y "%M59_OGRE_BIN%\x86\Release\*" "%OGRE_PATCH_FOLDER%\x86\"
+		)
+		IF EXIST "%M59_OGRE_BIN%\x64\Release" (
+			echo Copying: %M59_OGRE_BIN%\x64\Release\...
+			copy /Y "%M59_OGRE_BIN%\x64\Release\*" "%OGRE_PATCH_FOLDER%\x64\"
+		)
+	)
+	IF EXIST "%M59_OGRE_PATCHER_BIN%" (
+		echo Copying: %M59_OGRE_PATCHER_BIN%\...
+		copy /Y "%M59_OGRE_PATCHER_BIN%\*.exe" "%OGRE_PATCH_FOLDER%\"
+	)
+	IF EXIST "%M59_OGRE_SETUP_BIN%" (
+		echo Copying: %M59_OGRE_SETUP_BIN%\...
+		copy /Y "%M59_OGRE_SETUP_BIN%\*.exe" "%OGRE_SETUP_FOLDER%\"
+	)
+
+	pause
+
